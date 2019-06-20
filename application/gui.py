656f87e770f6561529209344083f4163f199bcf1
cece.cai <cece.cai@jfz.com>
2019-06-20 14:17:42 +0800

    add windows exe

--- application/gui.py
+++ application/gui.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python
 #_*_coding:utf-8_*_
 import os
-import sys
 import tkinter
 from tkinter.filedialog import askopenfilename
-from PIL import Image, ImageEnhance
+from PIL import Image
 from pytesseract import pytesseract
 
 CUR_DIR   = os.path.dirname(os.path.abspath(__file__))
 LANG_DIR  = CUR_DIR + '/' + 'lang'
 BIN_DIR   = CUR_DIR + '/' + 'bin'
 MAC_BIN   = BIN_DIR + '/' + 'macos/tesseract'
-WIN_BIN   = BIN_DIR + '/' + 'win/tesseract'
+WIN_BIN   = BIN_DIR + '/' + 'win/Tesseract-OCR/tesseract'
 LINUX_BIN = BIN_DIR + '/' + 'linux/tesseract'
 
-pytesseract.tesseract_cmd = MAC_BIN
+pytesseract.tesseract_cmd = WIN_BIN
 os.environ['TESSDATA_PREFIX'] = LANG_DIR
 
 def testPil(filepath):
